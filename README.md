b5b869daa7849f685c88a44703490f304174ce85
Georgios Kalantzis <gk18@sanger.ac.uk>
2023-09-11 09:36:22 +0100

    switch to bcftools query

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ In brief, the following work is undertaken:
 2. Variant annotation and encoding of additive/recessive genotypes. This is in accordance with [call_chets](https://github.com/frhl/call_chets). Annotation follows the [BRaVa guidelines](https://docs.google.com/document/d/11Nnb_nUjHnqKCkIB3SQAbR6fl66ICdeA-x_HyGWsBXM/edit).
 3. Testing for recessive effects. *Future work*
 
+### Notes
+* In `recessive_encoding/run_call_chets.sh`, the `cpp_dir/get_non_ref_sites` does almost the same as the proceeding line with bcftools, but the latter is safer to use and probably faster. 
+* Occasionally, I've noted that some parts of the recessive encoding pipeline crash, giving an something crashes occasionally with throwing an "OSError: [Errno 12] Cannot allocate memory". Can't find why this happens, but re-running the corresponding task solves the problem.
--- recessive_encoding/run_call_chets.sh
+++ recessive_encoding/run_call_chets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#TODO: add feature for filtering based on PP
+
 cpp_dir="/software/team281/gk18/call_chets/"
 module load common-apps/bcftools/1.18
 
@@ -8,7 +10,7 @@ if [[ -z $1 ]]; then
 else
     CHR=$1
 fi
-echo "Will create VCFs with biallelic encodings for chr-$chr."
+echo "Will create VCFs with biallelic encodings for chr-$CHR."
 
 work_dir="/FIXTHIS/phasing"
 out_dir="$work_dir/recessive_encoding/vcf_ready"
@@ -17,30 +19,27 @@ BCF="$work_dir/phased_genotypes_rare/GNH_39k.chr$CHR.phased_rare.no100trios.bcf"
 annot="/FIXTHIS/annotations/brava_GNH_44k_chr$CHR.txt"
 genotypes="$tmp_dir/chr$CHR.nonref.txt.gz"
 
+# if [ ! -f $tmp_dir/chr$CHR.phased_maxmaf01.vcf.gz ]; then 
 if [ ! -f $genotypes ]; then 
     echo "Generating a file with all genotypes..."
-    bcftools query $BCF --list samples > $tmp_dir/samples.txt
-    # bcftools view $BCF --max-af 0.01 -Ou | bcftools query -i'GT="alt"' -f'[%SAMPLE %CHROM:%POS:%REF:%ALT %GT\n]' | gzip > $genotypes
+    # bcftools query $BCF --list samples > $tmp_dir/samples.txt
 
-    bcftools view --max-af 0.01 $BCF -Oz -o $tmp_dir/chr$CHR.phased_maxmaf01.vcf.gz
-    $cpp_dir/get_non_ref_sites $tmp_dir/chr$CHR.phased_maxmaf01.vcf.gz $genotypes
+    bcftools view $BCF --max-af 0.01 -Ou | bcftools query -i'GT="alt"' -f'[%SAMPLE %CHROM:%POS:%REF:%ALT %GT\n]' | gzip > $genotypes
+    # bcftools view --max-af 0.01 $BCF -Oz -o $tmp_dir/chr$CHR.phased_maxmaf01.vcf.g
+    # $cpp_dir/get_non_ref_sites $tmp_dir/chr$CHR.phased_maxmaf01.vcf.gz $genotypes
 fi
-
-# grep var brava_GNH_44k_chr22.txt | awk '{print $1}' > example.genes.txt
-# rm gene_map.txt
-# cat example.genes.txt | while read gene; do grep $gene $annot| head -1 | tr ' ' '\n' | awk -v g=$gene 'NR>2{print $1, g}' >> gene_map.txt; done
 
 for consq in pLoF damaging_missense synonymous; do
     python prepare_genemap -a $annot -c $consq -o $tmp_dir/gene_map.chr$CHR.$consq.txt
     $cpp_dir/call_chets -g $genotypes -m $tmp_dir/gene_map.chr$CHR.$consq.txt > $tmp_dir/chr$CHR.gen_all.$consq.txt
     for bial in cis chet hom; do
+        # this is just a progress message
         grep $bial $tmp_dir/chr$CHR.gen_all.$consq.txt > $tmp_dir/chr$CHR.$bial.$consq.txt
         tmp=$(wc -l $tmp_dir/chr$CHR.$bial.$consq.txt | cut -d' ' -f1)
         echo "$bial-$consq events found: $tmp"
     done
     $cpp_dir/encode_vcf -i $tmp_dir/chr$CHR.gen_all.$consq.txt -s $tmp_dir/samples.txt -m additive  | bgzip > $out_dir/chr$CHR.additive.$consq.vcf.gz
     $cpp_dir/encode_vcf -i $tmp_dir/chr$CHR.gen_all.$consq.txt -s $tmp_dir/samples.txt -m recessive | bgzip > $out_dir/chr$CHR.recessive.$consq.vcf.gz
-    # rm $out_dir/chr$CHR.gen_all.$consq.txt
 done
 
 # repeat for pLoF & damaging_missense
@@ -49,5 +48,7 @@ python prepare_genemap -a $annot -c pLoF damaging_missense -o $tmp_dir/gene_map.
 $cpp_dir/call_chets -g $genotypes -m $tmp_dir/gene_map.chr$CHR.$consq.txt > $tmp_dir/chr$CHR.gen_all.$consq.txt
 $cpp_dir/encode_vcf -i $tmp_dir/chr$CHR.gen_all.$consq.txt -s $tmp_dir/samples.txt -m additive  | bgzip > $out_dir/chr$CHR.additive.$consq.vcf.gz
 $cpp_dir/encode_vcf -i $tmp_dir/chr$CHR.gen_all.$consq.txt -s $tmp_dir/samples.txt -m recessive | bgzip > $out_dir/chr$CHR.recessive.$consq.vcf.gz
+    
+# rm $out_dir/chr$CHR.gen_all.*.txt
 
-# bsub -J biall_encoding.[1-22] -o biall_encoding.%I.%J -q normal -R"select[mem>4000] rusage[mem=4000]" -M4000 -n 1 bash run_call_chets.sh
+# bsub -J biall_encoding.[1-22] -o biall_encoding.%I.%J -q normal -R"select[mem>8000] rusage[mem=8000]" -M8000 -n 1 bash /nfs/users/nfs_g/gk18/SCRIPTS/GNH_BRaVa_dev_misc/recessive_encoding/run_call_chets.sh
