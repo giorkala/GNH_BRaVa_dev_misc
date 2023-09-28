698cbca68d404b84db0b5d34d6b678aec0e599ec
Georgios Kalantzis <gk18@sanger.ac.uk>
2023-09-28 14:15:39 +0100

    setup for snakemake pipeline

---
+++ phasing/snakemake_pipeline/smk_02_phase_common.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#
+# phase common variants using Shapeit5
+# minimal script to be used within the Snakemake pipeline
+#
+# GK - Jul 14th, 2023, 26/9/23
+
+chr=$1 # number of chrom to process
+to_phase=$2 # the *preprocessed* BCF to phase
+tag=$3 # a file identifier
+
+### hyper-parameters ###
+module load common-apps/bcftools/1.16
+# module load HGI/common/shapeit/contig
+SHAPEIT_phase_common='/software/team281/bin/shapeit5/phase_common_static'
+threads=10
+gmap="FIXTHIS/genetic_maps/chr${chr}.b38.gmap.gz"
+out_prefix="phased_genotypes_common/$tag.phased_common.chr${chr}"
+phased="${out_prefix}.bcf"
+
+pbwt_modulo=0.1 # deault is 0.1, in shapeit4 with sequencing it is 0.0002
+pbwt_depth=4 # deault is 4
+pbwt_mac=5 # deafult is 5
+pbwt_mdr=0.1 # default is 0.1
+min_maf=0.001 # no default value
+
+if [ ! -f ${to_phase}.csi ]; then
+    echo "Generating the index for $to_phase..."
+    bcftools index $to_phase
+fi
+
+SECONDS=0
+
+if [[ -z $4 ]]; then
+    echo -e "\nPhasing without trio information.\n"
+    ${SHAPEIT_phase_common} \
+      --input ${to_phase} \
+      --map ${gmap} \
+      --region "chr${chr}" \
+      --thread ${threads} \
+      --output "${phased}" \
+      --log "${out_prefix}.log" \
+      --pbwt-modulo ${pbwt_modulo} \
+      --pbwt-depth ${pbwt_depth} \
+      --pbwt-mac ${pbwt_mac} \
+      --pbwt-mdr ${pbwt_mdr} \
+      --filter-maf ${min_maf} \
+      && echo "Finished phasing variants for chr${chr}, out: ${phased}, duration: ${SECONDS}" \
+      || echo "ERROR: Phasing variants failed for chr${chr}, duration: ${SECONDS}"
+
+else
+    echo -e "\nPhasing using the pedigree in $4.\n"
+    ${SHAPEIT_phase_common} \
+      --input ${to_phase} \
+      --map ${gmap} \
+      --region "chr${chr}" \
+      --thread ${threads} \
+      --output "${phased}" \
+      --log "${out_prefix}.log" \
+      --pbwt-modulo ${pbwt_modulo} \
+      --pbwt-depth ${pbwt_depth} \
+      --pbwt-mac ${pbwt_mac} \
+      --pbwt-mdr ${pbwt_mdr} \
+      --filter-maf ${min_maf} \
+      --pedigree $4 \
+      && echo "Finished phasing variants for chr${chr}, out: ${phased}, duration: ${SECONDS}" \
+      || echo "ERROR: Phasing variants failed for chr${chr}, duration: ${SECONDS}"
+fi
---
+++ phasing/snakemake_pipeline/smk_03_phase_rare.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+#
+# phase rare variants using SHAPEIT5
+# minimal script to be used within the Snakemake pipeline
+#
+# GK - Jul 21st, 2023, 26/9/23
+# based on https://github.com/frhl/wes_ko_ukbb/blob/main/scripts/phasing/phasing/02_phase_chunks
+#   and on https://odelaneau.github.io/shapeit5/docs/documentation/phase_rare/
+
+# genetic maps downloaded from https://github.com/odelaneau/shapeit5/tree/main/resources/maps
+#  chunk lists downloaded from https://github.com/odelaneau/shapeit5/tree/main/resources/chunks/b38/4cM
+# see README.md for more details
+
+chr=$1 # number of chrom to process
+phased_scaffold=$2 # the BCF of phased common variants
+to_phase=$3 # the *preprocessed* BCF to phase
+tag=$4 # just an identifier
+
+### hyper-parameters ###
+gmap="FIXTHIS/genetic_maps/chr${chr}.b38.gmap.gz"
+chunk_list="FIXTHIS/chunks_b38_4cM/chunks_chr$chr.txt"
+threads=5
+# we need to both deal with "invalid CONTIG" issues and obtain PPs for singletons. this is enabled by 'shapeit/contig'
+module load HGI/common/shapeit/contig
+module load common-apps/bcftools/1.16 
+
+### Output prefices ###
+work_dir="./"
+final_out="${work_dir}/phased_genotypes_rare/${tag}.phased_rare.chr${chr}.bcf"
+out_prefix="${work_dir}/phased_genotypes_rare/chunks/${tag}.phased_rare.chr${chr}"
+
+if [ ! -f ${to_phase}.csi ]; then
+    echo "Generating the index for $to_phase..."
+    bcftools index $to_phase
+fi
+
+mkdir -p ${work_dir}/phased_genotypes_rare/chunks/
+
+## paramters for phasing with shapeit
+phased_set_error="0.0001" # this is not needed 
+pbwt_min_mac=2 # for shapeit5r
+pbwt_depth=4 # 5
+pbwt_modulo=0.1 # default is 0.1 but 0.0004 ( 0.2 / 50 ) is default value when using --sequencing arugment
+pbwt_mdr=0.1
+pop_effective_size=15000
+
+SECONDS=0
+
+cat $chunk_list | while read LINE; do
+	CHK=$(echo $LINE | awk '{ print $1; }')
+	SRG=$(echo $LINE | awk '{ print $3; }')
+	IRG=$(echo $LINE | awk '{ print $4; }')
+
+    phased_chunk=$out_prefix.chunk$CHK.bcf
+    echo "Proceeding with chunk $CHK."
+
+    if [ ! -f ${phased_chunk} ]; then
+        phase_rare \
+            --input $to_phase \
+            --input-region $IRG \
+            --scaffold $phased_scaffold \
+            --scaffold-region $SRG \
+            --map $gmap \
+            --pbwt-mac $pbwt_min_mac \
+            --pbwt-depth-rare $pbwt_depth \
+            --pbwt-modulo $pbwt_modulo \
+            --pbwt-mdr $pbwt_mdr \
+            --effective-size $pop_effective_size \
+            --output $phased_chunk \
+            --thread $threads
+            # --phased-set-error $phased_set_error 
+    else
+        echo "Nothing to phase, $phased_chunk already exists."
+    fi
+done
+
+echo "Done with phasing, duration: ${SECONDS}."
+
+# second, concatenate the phased chunks
+if [ -f ${phased_chunk} ]; then
+    ls -1v $out_prefix.chunk*.bcf > $work_dir/sandbox/files.chr$chr
+    bcftools concat -n -Ob -o $final_out -f $work_dir/sandbox/files.chr$chr
+    bcftools index $final_out
+    rm $work_dir/sandbox/files.chr$chr
+fi
