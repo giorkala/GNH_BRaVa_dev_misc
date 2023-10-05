4e86f5b1c77d9bb17f34046f55c11c528e8a0891
Georgios Kalantzis <gk18@sanger.ac.uk>
2023-10-05 09:18:51 +0100

    fix issue in the SER assessment

--- phasing/snakemake_pipeline/smk_04_assess_phasing.sh
+++ phasing/snakemake_pipeline/smk_04_assess_phasing.sh
@@ -9,7 +9,8 @@ mode=$1
 chr=$2
 phased_true=$3
 phased_estd=$4
-out_prefix=$5
+pedigree=$5
+out_prefix=$6
 
 # module load HGI/common/shapeit/contig
 SHAPEIT_switch='/software/team281/bin/shapeit5/switch_static'
@@ -19,7 +20,9 @@ if [ $mode = "common" ]; then
 
     echo "Work for common variants at $out_prefix"
     # out_prefix="$out_dir/$tag.assess_common.chr$chr"
-    $SHAPEIT_switch --validation $phased_true --estimation $phased_estd --region chr$chr --output $out_prefix.assess_common.chr$chr --thread $threads
+    $SHAPEIT_switch --validation $phased_true --estimation $phased_estd \
+    --region chr$chr --pedigree ${work_dir}/GNH_${tag}.pedigree \
+    --output $out_prefix.assess_common.chr$chr --thread $threads
     zcat $out_prefix.assess_common.chr$chr.sample.switch.txt.gz | awk 'BEGIN { e=0; t=0; } { e+=$2; t+=$3; } END { print "SER =", e*100/t; }'
 
 elif [ $mode = "rare" ]; then
@@ -56,5 +59,5 @@ elif [ $mode = "getpp" ]; then
     bcftools view $BCF --max-af 0.0015 -Ou | bcftools query -i'GT="het"' -f'[%CHROM:%POS:%REF:%ALT %GT %PP \n]' | awk '($3>0.49)' | gzip > $out_prefix.chr$chr.maf00015.gz
 
 else
-    echo "Error: unrecognised argument: mode needs to be one of `common`,`rare`,`getpp`!"
+    echo "Error: unrecognised argument: mode needs to be one of `common`,`rare`,`getpp`."
 fi
